975d72a3b431e903da6b065bd60a81f07fe0acfa
Daniel <68309643+danielships@users.noreply.github.com>
2026-07-28 16:02:18 +0100

    feat: add URI API versioning, ClassSerializerInterceptor, OpenAPI export script, and architecture docs (#1084)
    Implement features assigned to danielships.
    
    Closes #1006
    Closes #1005
    Closes #1004
    Closes #1003

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType, ClassSerializerInterceptor } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
@@ -25,8 +25,14 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // Global prefix
+  // Global prefix & URI versioning
   app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   // Global validation pipe
   app.useGlobalPipes(
